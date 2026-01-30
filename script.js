9d4f12efca4d1794524331800b18bdb2d75457b6
Lekhit Patil <patillekhit@gmail.com>
2026-01-30 21:19:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+// Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href"))
       .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+// Active navbar highlight
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
   });
 });
